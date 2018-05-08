aa8dd20b64f1d9fae73c6e073239ca47f7cc4581
jeanpaul <jeanpaullm@github.com>
2018-05-08 17:27:24 -0600

    first commit

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/jeanpaul/Code/c++/Proyecto3Analisis
+# Build directory: /home/jeanpaul/Code/c++/Proyecto3Analisis/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("test")
+subdirs("benchmarks")
---
+++ cmake-build-debug/test/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jeanpaul/Code/c++/Proyecto3Analisis/test
+# Build directory: /home/jeanpaul/Code/c++/Proyecto3Analisis/cmake-build-debug/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(tester "/home/jeanpaul/Code/c++/Proyecto3Analisis/cmake-build-debug/test/tester")
